ec7014f62c3daca113f75866178db0d1d08d0ac4
Usuario <Usuario@DESKTOP-43M4CCK>
2024-10-03 23:34:03 -0300

    collections parte

--- tp5_grupo47/src/ar/edu/unju/escmi/tp5/collections/CollectionLibro.java
+++ tp5_grupo47/src/ar/edu/unju/escmi/tp5/collections/CollectionLibro.java
@@ -1,5 +1,26 @@
 package ar.edu.unju.escmi.tp5.collections;
 
-public class CollectionLibro {
+import ar.edu.unju.escmi.tp5.dominio.Libro;
+import ar.edu.unju.escmi.tp5.exceptions.LibroNoEncontradoException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionLibro {
+    public static List<Libro> libros = new ArrayList<>();
+
+    // agregar un libro
+    public static void agregarLibro(Libro libro) {
+        libros.add(libro);
+    }
+
+    // obtener un libro por su título
+    public static Libro obtenerLibro(String titulo) throws LibroNoEncontradoException {
+        for (Libro libro : libros) {
+            if (libro.getTitulo().equalsIgnoreCase(titulo)) {
+                return libro;
+            }
+        }
+        throw new LibroNoEncontradoException("Libro no encontrado: " + titulo);
+    }
 }
