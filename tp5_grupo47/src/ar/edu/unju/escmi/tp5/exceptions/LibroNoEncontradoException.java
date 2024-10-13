503ec6b588c1251f8a2165c558f23592b29b17a4
usuario <usuario@DESKTOP-84Q6FBK>
2024-10-12 22:33:55 -0300

    VivasMariaVictoria

--- tp5_grupo47/src/ar/edu/unju/escmi/tp5/exceptions/LibroNoEncontradoException.java
+++ tp5_grupo47/src/ar/edu/unju/escmi/tp5/exceptions/LibroNoEncontradoException.java
@@ -1,5 +1,6 @@
 package ar.edu.unju.escmi.tp5.exceptions;
 
+
 public class LibroNoEncontradoException extends Exception {
     
     private static final long serialVersionUID = 1L;
--- tp5_grupo47/src/ar/edu/unju/escmi/tp5/exceptions/UsuarioNoRegistradoException.java
+++ tp5_grupo47/src/ar/edu/unju/escmi/tp5/exceptions/UsuarioNoRegistradoException.java
@@ -2,6 +2,7 @@ package ar.edu.unju.escmi.tp5.exceptions;
 
 public class UsuarioNoRegistradoException extends Exception {
     
+	
     private static final long serialVersionUID = 1L;
 
     public UsuarioNoRegistradoException(String errorMessage) {
